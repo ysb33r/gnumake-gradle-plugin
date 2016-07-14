7822dd61a9c578d9a10062909bd02b758e4086d2
Schalk W. Cronjé <ysb33r@gmail.com>
2016-07-14 19:13:29 +0100

    Made an adjustment to cope with Windows output

--- src/test/groovy/org/ysb33r/gradle/gnumake/RunMakeSpec.groovy
+++ src/test/groovy/org/ysb33r/gradle/gnumake/RunMakeSpec.groovy
@@ -56,7 +56,7 @@ class RunMakeSpec extends Specification {
         String output = systemOut.toString()
 
         expect:
-        output.contains('fake-make build install DESTDIR=foo/bar') || output.contains('fake-make.bat build install DESTDIR=foo/bar')
+        output =~ /.*fake-make(\.bat)?\s+build\s+install\s+DESTDIR=foo\/bar.*/
         output.contains('INCAPATH=PERU')
     }
 }
